46f4396da3b59cc2fb3caa031dc84d89ac067535
iceman <1064839051@qq.com>
2017-12-27 13:41:14 +0800

    更新

---
+++ Vdocs/src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import marked from 'marked'
+import './md.css'
+Vue.prototype.$http = axios
+
+var rendererMD = new marked.Renderer()
+marked.setOptions({
+  renderer: rendererMD,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+}) // 基本设置
+
+console.log(marked('I am using __markdown__.'))
+// Outputs: <p>I am using <strong>markdown</strong>.</p>
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
